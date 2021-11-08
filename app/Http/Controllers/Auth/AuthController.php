f7f2c5f814818ef451fdd99b5b7a8b9e648c12aa
unknown <gasparkenken55@gmail.com>
2021-11-08 20:44:49 +0800

    first commit

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\mail\verificationEmail;
+use App\Mail\verificationEmail;
 use App\Models\Admin;
 use Illuminate\Http\Request;
 use App\Models\User;
