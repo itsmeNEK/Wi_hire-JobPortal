25bfd5fe34a87653a651d6f77caa84e50fd718e4
unknown <gasparkenken55@gmail.com>
2021-11-11 01:13:02 +0800

    new nov 10

--- app/Http/Middleware/secureheader.php
+++ app/Http/Middleware/secureheader.php
@@ -30,7 +30,7 @@ class secureheader
         // $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
         $response->headers->set('X-Content-Type-Options', 'nosniff');
         $response->headers->set('X-XSS-Protection', '1; mode=block');
-        $response->headers->set('X-Frame-Options', 'DENY');
+        // $response->headers->set('X-Frame-Options', 'DENY');
         $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
         return $response;
     }
