53b2fef85af5727861f4638f203239066d4dd777
unknown <gasparkenken55@gmail.com>
2022-02-24 20:52:18 +0800

    new

--- public/js/autolink.js
+++ public/js/autolink.js
@@ -1,10 +1,16 @@
 let textid = document.getElementById("body").innerHTML;
 
-function urlify(text) {
-    var urlRegex = /(https?:\/\/[^\s]+)/g;
-    return text.replace(urlRegex, function(url) {
-        return '<a target="_blank" href="' + url + '">' + url + '</a>';
-    })
+function replaceURLs(textid) {
+    if (!textid) return;
+
+    var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
+    return textid.replace(urlRegex, function(url) {
+        var hyperlink = url;
+        if (!hyperlink.match('^https?:\/\/')) {
+            hyperlink = 'http://' + hyperlink;
+        }
+        return '<a href="' + hyperlink + '" target="_blank" rel="noopener noreferrer">' + url + '</a>'
+    });
 }
-var html = urlify(textid);
+var html = replaceURLs(textid);
 document.getElementById("body").innerHTML = html;
